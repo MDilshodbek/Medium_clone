0e54d079dfea1d96ce882b7f2924539d0cc95693
MDilshodbek <mdbek55@gmail.com>
2026-02-16 16:50:17 +0900

    feat: developed get usersList and chosenUser api

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework import viewsets
 from rest_framework import permissions
 from .permissions import IsAuthorOrReadOnly
 # Create your views here.
@@ -14,3 +16,11 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
