b146d86d39ab246096c74503abcbd37f5439ab8b
MDilshodbek <mdbek55@gmail.com>
2026-02-16 22:55:11 +0900

    feat: developed APIs with ViewSet and Router

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import PostList, PostDetail, UserList, UserDetail
+# from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet, UserViewSet
 
-urlpatterns = [
-    path("users/", UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path("", PostList.as_view(), name="post-list"),
-    path("<int:pk>/", PostDetail.as_view(), name="post-detail"),
-]
+# urlpatterns = [
+#     path("users/", UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path("", PostList.as_view(), name="post-list"),
+#     path("<int:pk>/", PostDetail.as_view(), name="post-detail"),
+# ]
+
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
