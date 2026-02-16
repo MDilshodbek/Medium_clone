502e2609e3b33d096aae0058ee91a41e4845e4a2
MDilshodbek <mdbek55@gmail.com>
2026-02-17 00:26:55 +0900

    feat: created Rest API documentations using swagger and redoc

--- blogPosting/urls.py
+++ blogPosting/urls.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+# from rest_framework.schemas import get_schema_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Blog API',
+        description = 'Oddiy blog project APIs',
+        default_version = 'v1',
+        terms_of_service = 'https://www.google.com/policies/terms/',
+        contact = openapi.Contact(email='admin99nn@gmail.cum'),
+        license = openapi.License(name='License of Medium project'),
+    ),
+    public = True,
+    permission_classes = (permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +25,11 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/allauth/', include('allauth.urls')),
+    # path('openapi', get_schema_view(
+    #     title='Blog API', 
+    #     description='Medium clone project to learn Django rest framework', 
+    #     version='1.0.0',
+    #     ), name='openapi-schema'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
